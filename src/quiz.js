ceb091aa36fad4b0a833c6fe216a622d3c7a7741
AlexHerbertGit <alex.herbert.tech@gmail.com>
2024-03-13 12:01:43 +1300

    Created Questions variable and added question information

--- src/quiz.js
+++ src/quiz.js
@@ -1,2 +1,22 @@
 //Install Node.js
 
+// Install Readline - Later
+
+const question = [
+    {
+        question : "What is 2 + 2?",
+        options : ["2", "3", "4", "5"],
+        answer : "4"
+    },
+    {
+        question : "What is the captial of New Zealand?",
+        options : ["Auckland", "Wellington", "Nelson", "Christchurch"],
+        answer : "Wellington"
+    }
+    //Add more questions here.
+];
+
+//Declare Quiz Function
+function quiz() {
+    
+}
