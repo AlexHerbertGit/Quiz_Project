eaa7c2286d7d1b1e618851969fef84ba5b62a8f6
AlexHerbertGit <alex.herbert.tech@gmail.com>
2024-03-20 12:35:52 +1300

    Completed Quiz code

--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,7 @@
 //Install Node.js
 
 // Install Readline - Later
+const readlineSync = require('readline-sync');
 
 const questions = [
     { //Index 0
@@ -21,7 +22,28 @@ function quiz() {
     let score = 0;
 
     //Iterate through questions
-    questions.ForEach((q, index) => {
-        console.log(`Question ${index + 1}: ${q.question}`);
+    questions.forEach((q, index) => {
+        console.log(`Question ${index + 1}: ${q.question}`);//Displays the Question number and Question.
+        q.options.forEach((option, i) => {
+            console.log(`${i + 1}. ${option}`);//Displays question options
+        })
+    
+        //Request User Input via readline
+        let userAnswer = readlineSync.question("Your answer (number): ");
+        let answerIndex = parseInt(userAnswer, 10) - 1;
+
+        if(answerIndex < 0 || answerIndex > q.options.length) { //Check this later
+            console.log(`Please select an option for 1 to ${q.options.length}`);
+            } else if(q.options[answerIndex] === q.answer) {
+                console.log("Correct!");
+                score++;
+            } else {
+            console.log("Wrong!");
+        }
     })
-}
+    console.log(`Your final score is: ${score}/${questions.length}`);
+};
+
+quiz();
+
+
